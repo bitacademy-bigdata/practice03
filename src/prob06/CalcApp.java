30ad6d31daeead89ac29e701dad84892b6462743
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-08-02 17:09:27 +0900

    solved

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -18,19 +18,51 @@ public class CalcApp {
 			}
 			
 			String[] tokens = expression.split( " " );
+			if( tokens.length != 3 ) {
+				System.out.println( ">> 알 수 없는 식입니다." );
+				continue;
+			}
+			
 			int a = Integer.parseInt( tokens[0] );
-			String operator = tokens[1];
 			int b = Integer.parseInt( tokens[2] );
 			
-			switch( operator ) {
-				case "+" : 
-					/*Add add = new Add();
+			switch( tokens[ 1 ] ) {
+				case "+" : {
+					Add add = new Add();
 					add.setValue( a, b );
 					int result = add.calculate();
-					*/
+					System.out.println( ">> " + result );	
 					
 					break;
-					case "-" :
+				}
+				case "-" : {
+					Sub sub = new Sub();
+					sub.setValue( a, b );
+					int result = sub.calculate();
+					System.out.println( ">> " + result );	
+					
+					break;					
+				}
+				case "*" : {
+					Mul mul = new Mul();
+					mul.setValue( a, b );
+					int result = mul.calculate();
+					System.out.println( ">> " + result );	
+					
+					break;	
+				}
+				case "/" : {
+					Div div = new Div();
+					div.setValue( a, b );
+					int result = div.calculate();
+					System.out.println( ">> " + result );	
+					
+					break;
+				}
+				default : {
+					System.out.println( ">> 알 수 없는 연산입니다." );
+					break;
+				}
 				
 			}
 		}
